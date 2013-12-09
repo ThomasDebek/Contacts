2c9de86311309035b3c50f3373b7479ba12f77ef
ThomasDebek <tom.debek@gmail.com>
2013-12-09 10:47:16 +0100

    adding api

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Contacts::Application.config.secret_key_base = '5f018c5b5a1354786f0708089ac61525fb172eeae91ed8c3f88acfab95aca74e2cc9d890bcfcb2f9a6e105cb7d516f9994d3fd4d96d1e2c2aed1d39bfcbee8c4'
