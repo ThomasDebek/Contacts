e06a99108ce702cec86b94802f8a3b5fa0921ba6
ThomasDebek <tom.debek@gmail.com>
2013-12-09 11:26:37 +0100

    Adding controller, change routing

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,5 @@
+class HelloController < ApplicationController
+  def world
+    @msg =  "Hello world!"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Contacts::Application.routes.draw do
+
+  root to: 'hello#world'
+
+
+  get "hello/world"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
